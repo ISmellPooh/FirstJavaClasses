c42947bbce4ba6e93af119ea95bdc380f6188fd7
droberts19 <droberts19@scienceleadership.org>
2018-09-17 15:02:11 -0400

    commit

--- src/org/sla/Plane.java
+++ src/org/sla/Plane.java
@@ -1,10 +1,20 @@
 package org.sla;
 
 class Plane {
-    int color;
     int fuelLoad;
-    int maxPassengerCount;
+    int passengerCount;
     int cost;
+    String vehicleType;
+    String color;
     String origin;
-    String destination
+    String destination;
+
+    void describeThing() {
+        System.out.println("This " + vehicleType + " has a gorgeous " + color + " paint job.");
+        System.out.println("It has a max fuel load of " + fuelLoad + " gallons.");
+        System.out.println("It has a passenger capacity of " + passengerCount + ".");
+        System.out.println("Finally, it costs $" + cost + ".");
+    }
+
+    void tripStatus() {}
 }
