46978ecbc7db0cb335ef33657d5745784ffbcaae
droberts19 <droberts19@scienceleadership.org>
2018-09-20 10:26:37 -0400

    commit

--- src/org/sla/MakeTheThingsDoStuff.java
+++ src/org/sla/MakeTheThingsDoStuff.java
@@ -6,15 +6,20 @@ class MakeTheThingsDoStuff {
 
         Plane plane1 = new Plane();
         plane1.modelNumber = "Boeing 747";
-        plane1.vehicleType = "Aircraft";
-        plane1.color = "Blue";
-        plane1.fuelLoad = 48455;
+        plane1.vehicleType = "aircraft";
+        plane1.color = "blue";
+        plane1.fuelLoad = 48445;
+        plane1.range = 7260;
         plane1.speed = 570;
         plane1.passengerCount = 524;
         plane1.cost = 24000000;
         plane1.origin = "Philadelphia";
         plane1.destination = "Los Angeles";
+        plane1.travelDistance = 2396;
 
+        System.out.println();
         plane1.describe();
+        System.out.println();
+        plane1.tripStatus();
     }
 }
