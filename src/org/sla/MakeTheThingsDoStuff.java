fb22ec1a051163a1adf2ca472e5de34f73cadb5f
ISmellPooh <egorski19@scienceleadership.org>
2018-09-24 14:45:49 -0400

    bgycdrvgh nhjvyrttyubg

--- src/org/sla/MakeTheThingsDoStuff.java
+++ src/org/sla/MakeTheThingsDoStuff.java
@@ -17,30 +17,43 @@ class MakeTheThingsDoStuff {
         plane1.destination = "Los Angeles";
         plane1.travelDistance = 2396;
 
-        Plane plane2 = new Plane();
-        plane2.modelNumber = "F 16";
-        plane2.vehicleType = "aircraft";
-        plane2.color = "grey";
-        plane2.fuelLoad = 840;
-        plane2.range = 1740;
-        plane2.speed = 1500;
-        plane2.passengerCount = 2;
-        plane2.cost = 14600000;
-        plane2.origin = "New York";
-        plane2.destination = "Miami";
-        plane2.travelDistance = 1094;
-
         System.out.println();
         plane1.describe();
         System.out.println();
         plane1.tripStatus();
-        System.out.println();
-        plane1.makePlane();
-        System.out.println();
-        plane2.describe();
-        System.out.println();
-        plane2.tripStatus();
-        System.out.println();
-        plane2.makePlane();
+
+        Car car1 = new Car();
+        car1.carBrand = "Plymouth";
+        car1.carModel = "Barracuda";
+        car1.yearMade = 1971;
+        car1.sizeOfEngine = "V8";
+        car1.horsePower = 440;
+
+        car1.describeCar();
+        car1.carAge();
+
+        Car car2 = new Car();
+        car2.carBrand = "Dodge";
+        car2.carModel = "Charger";
+        car2.yearMade = 2018;
+        car2.sizeOfEngine = "V8";
+        car2.horsePower = 707;
+
+        car2.describeCar();
+        car2.carAge();
+
+        Car car3 = new Car();
+        car3.carBrand = "Bugatti";
+        car3.carModel = "Veyron";
+        car3.yearMade = 2011;
+        car3.sizeOfEngine = "V16";
+
+        Car mostPowerfulCar = car1;
+        if (car2.horsePower > car1.horsePower) {
+            mostPowerfulCar = car2;
+        }
+        if (car3.horsePower > car1.horsePower & car3.horsePower > car2.horsePower) {
+            mostPowerfulCar = car3;
+        }
     }
 }
